e26765381cfb490cca0440e314469bb0ed546f5d
Mychel Lima <mychelhenrique321@gmail.com>
2022-09-01 16:51:37 -0300

    crud-laravel

---
+++ Crud_laravel/hdcevents/resources/views/events/create.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.main')
+
+@section('title', 'Criar Evento')
+
+@section('content')
+    <div id="event-create-container" class="col-md-6 offset-md-3">
+        <h1>Crie seu evento</h1>
+        <form action="/events" method="POST" enctype="multipart/form-data">
+            @csrf
+            <div class="form-group">
+                <label for="img">imagem do evento:</label>
+                <input type="file" id="image" name="image" class="form-control-file">   
+            </div>
+            <div class="form-group">
+                <label for="title">Evento:</label>
+                <input type="text" class="form-control" id="title" name="title" placeholder="nome do evento...">             
+            </div>
+            <div class="form-group">
+                <label for="date">Data do evento:</label>
+                <input type="date" class="form-control" id="date" name="date" >             
+            </div>
+            <div class="form-group">
+                <label for="title">Cidade:</label>
+                <input type="text" class="form-control" id="city" name="city" placeholder="local do evento...">             
+            </div>
+            <div class="form-group">
+                <label for="title">o evento é privado?:</label>
+                <select name="private" id="private" class="form-control">
+                    <option value="0">Não</option>
+                    <option value="1">Sim</option>
+                </select>
+                         
+            </div>
+            <div class="form-group">
+                <label for="title">Descrição:</label>
+                <textarea name="description" id="description" class="form-control" placeholder="o que vai acontecer no evento?"></textarea>           
+            </div>
+            <div class="form-group">
+                <label for="title">Adicione itens de infraestrutura:</label>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="cadeiras"> cadeiras
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="palco"> palco
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="cerveja gratis"> cerveja gratis
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="open food"> open food
+                </div>
+                <div class="form-group">
+                    <input type="checkbox" name="items[]" value="brindes"> brindes
+                </div>
+                           
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Criar evento">
+            </div>
+            
+        </form>
+    </div>
+
+@endsection
